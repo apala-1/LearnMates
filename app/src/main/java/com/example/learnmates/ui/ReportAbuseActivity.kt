965d396258e0e15af3aebb655d0d0a512fc7eff9
Apala Lamichhane <145183257+apalalamichhane@users.noreply.github.com>
2025-01-01 16:09:04 +0545

    Sprint 3 FrontEnd Tasks

---
+++ app/src/main/java/com/example/learnmates/ui/ReportAbuseActivity.kt
@@ -0,0 +1,21 @@
+package com.example.learnmates.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.learnmates.R
+
+class ReportAbuseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_report_abuse)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
