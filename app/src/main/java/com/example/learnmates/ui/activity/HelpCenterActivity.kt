ee6ac0065eaa1f283bddc2ff0ce8ab29c1a7f509
Apala Lamichhane <145183257+apalalamichhane@users.noreply.github.com>
2025-01-06 13:56:55 +0545

    Solved internal HelpCenter errors

--- app/src/main/java/com/example/learnmates/ui/activity/HelpCenterActivity.kt
+++ app/src/main/java/com/example/learnmates/ui/activity/HelpCenterActivity.kt
@@ -1,31 +1,54 @@
 package com.example.learnmates.ui.activity
 
+import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.learnmates.R
+import com.example.learnmates.databinding.ActivityHelpCenterBinding
 
 class HelpCenterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHelpCenterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_help_center)
+        binding = ActivityHelpCenterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        var gettingStarted = findViewById<TextView>(R.id.gettingStarted)
-        var FAQs = findViewById<TextView>(R.id.faqs)
-        var contactSupport = findViewById<TextView>(R.id.contactSupport)
-        var PrivacyAndSecurity = findViewById<TextView>(R.id.privacyandsecurity)
-        var CommunityGuildelines = findViewById<TextView>(R.id.communityguidelines)
-        var reportAbuse = findViewById<TextView>(R.id.reportabuse)
-
-        gettingStarted.setOnClickListener{
-
+        binding.gettingStarted.setOnClickListener {
+            val intent = Intent(this@HelpCenterActivity, GettingStartedActivity::class.java)
+            startActivity(intent)
         }
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        binding.faqs.setOnClickListener {
+            val intent = Intent(this@HelpCenterActivity, FAQsActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.contactSupport.setOnClickListener {
+            val intent = Intent(this@HelpCenterActivity, ContactSupportActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.privacyandsecurity.setOnClickListener {
+            val intent = Intent(this@HelpCenterActivity, PrivacyAndSecurityActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.communityguidelines.setOnClickListener {
+            val intent = Intent(this@HelpCenterActivity, CommunityGuidelinesActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.reportabuse.setOnClickListener {
+            val intent = Intent(this@HelpCenterActivity, ReportAbuseActivity::class.java)
+            startActivity(intent)
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
