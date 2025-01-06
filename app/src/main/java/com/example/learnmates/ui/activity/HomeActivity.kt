e1a3d098460d3c9f3ca628c4c978b0b6e27f803b
Apala Lamichhane <145183257+apalalamichhane@users.noreply.github.com>
2025-01-06 12:32:02 +0545

    Added BottomNavigationView in home and linked it to respective fragments

--- app/src/main/java/com/example/learnmates/ui/activity/HomeActivity.kt
+++ app/src/main/java/com/example/learnmates/ui/activity/HomeActivity.kt
@@ -5,17 +5,47 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.example.learnmates.R
+import com.example.learnmates.databinding.ActivityHomeBinding
+import com.example.learnmates.ui.fragment.HomePageFragment
+import com.example.learnmates.ui.fragment.ProfileFragment
 
 class HomeActivity : AppCompatActivity() {
+    lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        replaceFragment(HomePageFragment())
+
+        binding.bottomView.setOnItemSelectedListener {
+            when (it.itemId){
+                R.id.navHome -> replaceFragment(HomePageFragment())
+                R.id.navProfile -> replaceFragment(ProfileFragment())
+                else -> { }
+            }
+            true
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager: FragmentManager =
+            supportFragmentManager
+
+        val fragmentTransaction: FragmentTransaction =
+            fragmentManager.beginTransaction()
+
+        fragmentTransaction.replace(R.id.frame, fragment)
+        fragmentTransaction.commit()
     }
 }
