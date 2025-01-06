f6380d1da5bd500fe8f12d26876c9fd8ab492956
Apala Lamichhane <145183257+apalalamichhane@users.noreply.github.com>
2025-01-06 11:41:04 +0545

    Constraint Error fixed in ForgotPassActivity

--- app/src/main/java/com/example/learnmates/ui/activity/ForgotPassActivity.kt
+++ app/src/main/java/com/example/learnmates/ui/activity/ForgotPassActivity.kt
@@ -1,6 +1,8 @@
 package com.example.learnmates.ui.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,10 +10,23 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.learnmates.R
 
 class ForgotPassActivity : AppCompatActivity() {
+    lateinit var backBtn: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_forgot_pass)
+
+        backBtn = findViewById(R.id.backBtn)
+
+        backBtn.setOnClickListener{
+            val intent : Intent =
+                Intent(
+                    this@ForgotPassActivity,
+                    FrontPageActivity::class.java
+                )
+            startActivity(intent)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
