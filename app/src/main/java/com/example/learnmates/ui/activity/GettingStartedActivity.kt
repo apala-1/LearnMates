566cd8600c21a798f8e749b02b1135d975ad90ed
Apala Lamichhane <145183257+apalalamichhane@users.noreply.github.com>
2025-01-06 14:13:38 +0545

    Solved Constraints Problem in GettingStartedActivity

--- app/src/main/java/com/example/learnmates/ui/activity/GettingStartedActivity.kt
+++ app/src/main/java/com/example/learnmates/ui/activity/GettingStartedActivity.kt
@@ -1,17 +1,36 @@
 package com.example.learnmates.ui.activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.learnmates.R
+import com.example.learnmates.databinding.ActivityGettingStartedBinding
 
 class GettingStartedActivity : AppCompatActivity() {
+    lateinit var binding: ActivityGettingStartedBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_getting_started)
+        binding = ActivityGettingStartedBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnBack.setOnClickListener {
+            val intent = Intent(this@GettingStartedActivity,
+                            HelpCenterActivity::class.java
+                )
+            startActivity(intent)
+        }
+
+        binding.btnNext.setOnClickListener {
+            val intent = Intent(this@GettingStartedActivity,
+                        CommunityGuidelinesActivity::class.java
+                )
+            startActivity(intent)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
