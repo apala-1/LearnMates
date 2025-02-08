1a25eea83cfe0e05b271f91370ab6344a73f4ffc
Apala Lamichhane <145183257+apalalamichhane@users.noreply.github.com>
2025-02-08 11:38:05 +0545

    Kimti colors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,6 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.firebase.auth)
     implementation(libs.firebase.database.ktx)
-    implementation(libs.firebase.firestore.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -58,5 +57,6 @@ dependencies {
     implementation ("androidx.fragment:fragment-ktx:1.6.1")
     implementation ("com.github.bumptech.glide:glide:4.16.0")
     implementation ("com.google.firebase:firebase-storage:20.2.0")
+    implementation ("com.cloudinary:cloudinary-android:2.0.0")
 
 }
